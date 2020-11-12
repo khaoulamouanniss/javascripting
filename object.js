20cf56a2252e1e610c4022071d536327a51ed0f1
Khaoula <khaoula.mouanniss@gmail.com>
2020-11-12 18:04:59 +0000

    javasripting exerises

---
+++ object.js
@@ -0,0 +1,12 @@
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+/* Use console.log() to print the pizza object to the terminal.
+
+  Check to see if your program is correct by running this command:
+
+     javascripting verify objects.js*/
+
+console.log(pizza);
